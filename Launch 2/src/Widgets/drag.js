be412fd9583a7e59a4b93f9f806094903b98542f
Omar Waleed <145815287+OmarWaleed41@users.noreply.github.com>
2025-10-13 02:34:02 +0300

    Update drag.js
    Redefined the dragging logic for the widgets to add a new feature and a less messy logic

--- Launch 2/src/Widgets/drag.js
+++ Launch 2/src/Widgets/drag.js
@@ -2,22 +2,47 @@
 let isDragging = false;
 let lastX = 0;
 let lastY = 0;
+let mouseMoved = false;
 
-document.getElementById('dragLayer').addEventListener('mousedown', (e) => {
-    isDragging = true;
-    lastX = e.screenX;
-    lastY = e.screenY;
-});
+const dragLayer = document.getElementById('dragLayer');
 
-document.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
+function onMouseMove(e) {
     const dx = e.screenX - lastX;
     const dy = e.screenY - lastY;
-    window.chrome.webview.postMessage({ type: 'drag', dx, dy });
+
+    if (!mouseMoved && (Math.abs(dx) > 3 || Math.abs(dy) > 3)) {
+        mouseMoved = true;
+        isDragging = true;
+        console.log('dragging started');
+    }
+
+    if (isDragging) {
+        window.chrome.webview.postMessage({ type: 'drag', dx, dy });
+        lastX = e.screenX;
+        lastY = e.screenY;
+    }
+}
+
+function onMouseUp() {
+    if (!mouseMoved) {
+        try {
+            openInBrowser();
+    
+        }
+        catch (error) {
+            console.error('Error opening URL:', error);
+        }
+    }
+    isDragging = false;
+    mouseMoved = false;
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+}
+
+dragLayer.addEventListener('mousedown', (e) => {
     lastX = e.screenX;
     lastY = e.screenY;
-});
-
-document.addEventListener('mouseup', () => {
-    isDragging = false;
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
 });
