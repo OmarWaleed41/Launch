341e0cdcbe184ef2fd5d31acd416e4e472080829
Omar Waleed <145815287+OmarWaleed41@users.noreply.github.com>
2025-10-13 18:36:47 +0300

    Update weather.js
    the location used to be hard coded to cairo only now it uses an api to locate the city based on the ip address
    also added a click option to open the browser on msn weather to display extra weather data

--- Launch 2/src/Widgets/weather/weather.js
+++ Launch 2/src/Widgets/weather/weather.js
@@ -14,7 +14,12 @@ weather.className = 'widget';
 weather.id = 'weather';
 
 async function loadWidget() {
-    const response2 = await fetch("http://api.weatherapi.com/v1/current.json?key=1b7219347ef742b9b11181215232409&q=cairo");
+    const ipRes = await fetch("https://ipwho.is/");
+    const ipData = await ipRes.json();
+    let lon = ipData.longitude;
+    let lat = ipData.latitude;
+    console.log(" lon: " + lon + " lat: " + lat);
+    const response2 = await fetch(`http://api.weatherapi.com/v1/current.json?key=1b7219347ef742b9b11181215232409&q=${lon},${lat}&aqi=no`);
     const W_data = await response2.json();
 
     img.src = `https:${W_data["current"]["condition"]["icon"]}`;
@@ -25,3 +30,12 @@ async function loadWidget() {
     loadWidget();
     setInterval(loadWidget, 1800000);
 })();
+
+function openInBrowser() {
+    const url = 'https://weather.com/';
+  if (window.chrome?.webview) {
+    window.chrome.webview.postMessage({ type: 'openBrowser', url });
+  } else {
+    window.open(url, '_blank');
+  }
+}
